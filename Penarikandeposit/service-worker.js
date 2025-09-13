165a1b947a094aa0cc9af7c0ec18d99a851a7a37
databaseee1 <tigohostt@gmail.com>
2025-09-13 07:23:36 +0700

    Tambah PWA support: service worker & manifest

---
+++ Penarikandeposit/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/icon-192.png",
+  "/icon-512.png",
+];
+
+// Install service worker & simpan cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ambil dari cache kalau offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache lama kalau ada versi baru
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
